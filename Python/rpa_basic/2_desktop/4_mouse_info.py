39d0e1d0994327742a2bea7cf6ccd02239836627
nataehoon <skeoddla@gmail.com>
2022-04-14 16:36:46 +0900

    changed path

--- Python/rpa_basic/2_desktop/4_mouse_info.py
+++ Python/rpa_basic/2_desktop/4_mouse_info.py
@@ -1,2 +1,8 @@
 import pyautogui
-pyautogui.mouseInfo()
+# pyautogui.FAILSAFE = False
+pyautogui.PAUSE = 1 # 모든 동작에 1초씩 sleep적용
+#pyautogui.mouseInfo()
+
+for i in range(5):
+    pyautogui.move(100, 100)
+    # pyautogui.sleep(1)
