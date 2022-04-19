dda27778d4fb3e04b5659f9d2508f2043eab0fb8
nataehoon <skeoddla@gmail.com>
2022-04-19 18:19:41 +0900

    update

--- Python/Python/exercise/webscraping_basic/12_selenium.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Chrome("./chromedriver.exe") # 경로 정보
-browser.get("http://naver.com")
--- Python/exercise/rpa_basic/1_exel/14_merge.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import Workbook
-wb = Workbook()
-ws = wb.active
-
-# 병합하기
-ws.merge_cells("B2:D2")
-ws["B2"].value = "Merged Cell"
-
-wb.save("sample_merge.xlsx")
